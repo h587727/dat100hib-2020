653a9e2ef3785f29ffce8d94ac5b3475d5a00acb
h587727 <54101626+h587727@users.noreply.github.com>
2020-09-01 23:03:10 +0200

    Første del av Innlevering

--- src/no/hvl/dat100/Oblig1Oppg5.java
+++ src/no/hvl/dat100/Oblig1Oppg5.java
@@ -1,11 +1,12 @@
 package no.hvl.dat100;
 
-public class Oblig1Oppg4 {
+public class Oblig1Oppg5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-
+		
+		System.out.println("Hello World");
 	}
+	
 
 }
